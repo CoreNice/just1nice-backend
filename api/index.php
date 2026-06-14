6cd579a1eea48bbabd62cadc7db13ad08e47684b
Justin Nice <justinviar200@gmail.com>
2026-06-15 02:50:23 +0700

    Vercel

--- api/index.php
+++ api/index.php
@@ -2,6 +2,12 @@
 
 $_SERVER['HTTP_ACCEPT'] = $_SERVER['HTTP_ACCEPT'] ?? 'application/json';
 
+$_ENV['APP_BASE_PATH'] = dirname(__DIR__);
+
+if (! is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0755, true);
+}
+
 if (! str_contains($_SERVER['HTTP_ACCEPT'], 'application/json')) {
     $_SERVER['HTTP_ACCEPT'] = 'application/json';
 }
